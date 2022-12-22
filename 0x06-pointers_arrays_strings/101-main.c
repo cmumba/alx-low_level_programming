9585d8677bb749878d3a0ca0ce762975454014f3
cmumba <118109635+cmumba@users.noreply.github.com>
2022-12-22 23:30:29 +0200

    Update 101-main.c

--- 0x06-pointers_arrays_strings/101-main.c
+++ 0x06-pointers_arrays_strings/101-main.c
@@ -1,11 +1,13 @@
 #include "main.h"
-
 /**
  * main - check the code
  * Return: Always 0.
  */
+
 int main(void)
+
 {
+
 	print_number(98);
 	_putchar('\n');
 	print_number(402);
@@ -16,5 +18,6 @@ int main(void)
 	_putchar('\n');
 	print_number(-98);
 	_putchar('\n');
+
 	return (0);
 }
